266906a69bf124bb72c02e88ab8e5636111152b8
Manu88x <emmanuelokoth112@gmail.com>
2024-11-14 23:18:04 +0300

    added contact information in conact in pages

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,3 +1,18 @@
-
+function Contact() {
+    return (
+      <div ClassName="Contacts">
+        <h2>Contact Us</h2>
+        <p>Feel free to reach out to us for more information on:</p>
+        <ul >
+          <li><a href="https://www.instagram.com/its_gumgum/" target="_blank">Instagram</a></li>
+          <li> <a href="https://support.google.com/mail/answer/8494?hl=en&co=GENIE.Platform%3DDesktop" target="_blank"><a/>Gmail:Emmanuelokoth@student.moringaschool.com</a> </li>
+          <li> <a href="https://web.whatsapp.com/" target="_blank">Whats up:0743580466<a/></a> </li>
+        </ul>
+      </div>
+    );
+  }
+  
+  export default Contact;
+  
   
   
