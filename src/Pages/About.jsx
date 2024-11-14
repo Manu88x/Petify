c1202c1964066c9fba1ed5cb0de546999965e340
Manu88x <emmanuelokoth112@gmail.com>
2024-11-14 23:14:41 +0300

    added About information in pages

---
+++ src/Pages/About.jsx
@@ -0,0 +1,24 @@
+function About() {
+    return (
+      <div ClassName="About">
+        <h2>About Us</h2>
+        <p>This page contains information about our app</p>
+
+        <p>This is  a web app that allows its users to keep track of list items they buy while shopping.</p>
+
+        <p>
+          You can:
+      <ul >
+        <li> Submit an item along with its  price and image. </li>
+        <li>Delete it if u see its too expensive or if u dont want it anymore.</li>
+         <li>Switch its position in a list by moving it up or down .</li>
+         <li>Theres also an addition of a timer for you to keep track of time at the store.</li>
+      </ul>
+        </p>
+
+      </div>
+    );
+  }
+  
+  export default About;
+  
