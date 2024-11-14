3413381677a0da17d307757a2742b2772df9c02f
Manu88x <emmanuelokoth112@gmail.com>
2024-11-14 23:20:16 +0300

    added new information in home in pages

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,2 +1,11 @@
-
+function Home() {
+    return (
+      <div className="Home">
+        <h2>Welcome to the Home Page</h2>
+        <p>Welcome to our Shopify app.</p>
+      </div>
+    );
+  }
+  
+  export default Home;
   
