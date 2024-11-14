36931579043d49c68d9d02bb1afaca4b39776845
Manu88x <emmanuelokoth112@gmail.com>
2024-11-15 00:10:19 +0300

    added some changes to contact in pages

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -5,8 +5,8 @@ function Contact() {
         <p>Feel free to reach out to us for more information on:</p>
         <ul >
           <li><a href="https://www.instagram.com/its_gumgum/" target="_blank">Instagram</a></li>
-          <li> <a href="https://support.google.com/mail/answer/8494?hl=en&co=GENIE.Platform%3DDesktop" target="_blank"><a/>Gmail:Emmanuelokoth@student.moringaschool.com</a> </li>
-          <li> <a href="https://web.whatsapp.com/" target="_blank">Whats up:0743580466<a/></a> </li>
+          <li> <a href="https://support.google.com/mail/answer/8494?hl=en&co=GENIE.Platform%3DDesktop" target="_blank"><a/>Emmanuelokoth@student.moringaschool.com</a> </li>
+          <li> <a href="https://web.whatsapp.com/" target="_blank">0743580466<a/></a> </li>
         </ul>
       </div>
     );
