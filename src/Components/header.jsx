aab48aeaf92af411c0f25e55ada4abf4a5906c8c
Manu88x <emmanuelokoth112@gmail.com>
2024-11-15 11:58:52 +0300

    code

--- src/Components/header.jsx
+++ src/Components/header.jsx
@@ -4,9 +4,9 @@ function Header(){
         <h1>Shopify</h1>
         <nav>
         <ul>
-                <li><a href="https://superb-gelato-afbe79.netlify.app/">Home</a></li>
-                <li><a href="https://superb-gelato-afbe79.netlify.app/about">About</a></li>
-                <li><a href="https://superb-gelato-afbe79.netlify.app/contact">contact</a></li>
+                <li><a href=" http://localhost:5173/">Home</a></li>
+                <li><a href=" http://localhost:5173/about">About</a></li>
+                <li><a href=" http://localhost:5173/contact">contact</a></li>
             </ul>
             <hr></hr>
         </nav>
