db9cfac370710c3c81cd52e22bfeb9a571e963b6
Manu88x <emmanuelokoth112@gmail.com>
2024-11-15 11:27:45 +0300

    changes to header

--- src/Components/header.jsx
+++ src/Components/header.jsx
@@ -4,7 +4,7 @@ function Header(){
         <h1>Shopify</h1>
         <nav>
             <ul>
-                <li><a href="http://localhost:5173/">Home</a></li>
+                <li><a href="https://superb-gelato-afbe79.netlify.app/">Home</a></li>
                 <li><a href="https://superb-gelato-afbe79.netlify.app/about">About</a></li>
                 <li><a href="https://superb-gelato-afbe79.netlify.app/contact">contact</a></li>
             </ul>
