7c429b9d4bb4eb2e678bcaff90f7140e61071bda
Manu88x <emmanuelokoth112@gmail.com>
2024-11-15 11:24:07 +0300

    cjhanges to header

--- src/Components/header.jsx
+++ src/Components/header.jsx
@@ -5,8 +5,8 @@ function Header(){
         <nav>
             <ul>
                 <li><a href="http://localhost:5173/">Home</a></li>
-                <li><a href="http://localhost:5173/about">About</a></li>
-                <li><a href="http://localhost:5173/contact">contact</a></li>
+                <li><a href="https://superb-gelato-afbe79.netlify.app/about">About</a></li>
+                <li><a href="https://superb-gelato-afbe79.netlify.app/contact">contact</a></li>
             </ul>
             <hr></hr>
         </nav>
